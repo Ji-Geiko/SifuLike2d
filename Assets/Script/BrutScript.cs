513714563b18369e42acc3ed201a214fa0f0bdee
Ji-Geiko <matteo.lohner@gmail.com>
2022-05-15 15:03:58 +0200

    add Move to Brut

--- Assets/Script/BrutScript.cs
+++ Assets/Script/BrutScript.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class BrutScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player;
+    public float closeRange = 1.5f;
+    public float[] attackRange = new float[2];
+    public float longRange  = 10f;
+    public string state = "idle";
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        attackRange[0] = 1.5f;
+        attackRange[1] = 2f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        if(distance >= attackRange[0] && distance <= attackRange[1])
+        {
+            print("attack");
+            state = "attack";
+        }
+        else if(distance < closeRange)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+            state = "close";
+        }
+        else if(distance < longRange)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+            state = "long";
+        }else
+        {
+            state = "idle";
+        }
+        print(Vector3.Distance(transform.position, player.transform.position));
     }
 }
